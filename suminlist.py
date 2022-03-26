09c98e4447b6743ef39f97deebb991c164afce63
AjayDhanwade <101167434+AjayDhanwade@users.noreply.github.com>
2022-03-26 09:30:16 +0530

    Add files via upload

---
+++ suminlist.py
@@ -0,0 +1,12 @@
+#sum of all the elements in the list
+
+def summetion(list1):
+    s=0
+    for i in range(len(list1)):
+        list1[i]=int(list1[i])
+        s=list1[i]+s
+    return s
+
+list1=[8,2,3,0,7]
+summ=summetion(list1)
+print(summ)
